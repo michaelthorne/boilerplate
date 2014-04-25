877a15b3ea676b89ca95e8d388d00cfa7d67f120
Michael Thorne <michael.thorne@gmail.com>
2014-04-25 12:24:24 +0200

    Grunt: added connect, less and watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,53 @@ module.exports = function (grunt)
 {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    livereload: true,
+                    open: true,
+                    port: 1337
+                }
+            }
+        },
         csslint: {
             strict: {
+                src: ['assets/css/main.css'],
                 options: {
                     import: 2
-                },
-                src: ['assets/css/main.css']
+                }
             }
         },
         jshint: {
             files: ['assets/js/main.js']
         },
+        less: {
+            build: {
+                files: {
+                    'assets/css/main.css': 'assets/less/main.less'
+                },
+                options: {
+                    cleancss: true
+                }
+            }
+        },
         validation: {
             files: {
                 src: ['*.html']
+            }
+        },
+        watch: {
+            build: {
+                files: [
+                    'assets/js/**',
+                    'assets/less/**',
+                    '*.html'
+                ],
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
             }
         }
     });
@@ -23,11 +56,16 @@ module.exports = function (grunt)
     var defaultTasks = [
         'csslint',
         'jshint',
+        'less',
         'validation'
     ];
 
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-html-validation');
     grunt.registerTask('default', defaultTasks);
+    grunt.registerTask('serve', ['less', 'connect', 'watch']);
 };
